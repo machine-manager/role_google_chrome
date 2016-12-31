567986c7713f4544d4569ec876fd4898721bb6a6
Ivan Kozik <ivan@ludios.org>
2016-12-31 05:05:05 +0000

    role_google_chrome

---
+++ lib/role.ex
@@ -0,0 +1,20 @@
+defmodule RoleGoogleChrome do
+	def role(_tags \\ []) do
+		%{
+			implied_roles:     [RoleGoogleChromeRepo],
+			desired_packages:  ["google-chrome-stable"],
+			ferm_output_chain:
+				"""
+				# Chrome tries to talk UPnP on all interfaces (probably for Chromecast); we don't want to log it
+				daddr 239.255.255.250 proto udp dport 1900 REJECT;
+
+				outerface lo {
+					# Chrome Developer Tools, when opened, repeatedly tries to
+					# connect to this; we don't want to log it.  Implemented in
+					# chrome/browser/devtools/device/devtools_android_bridge.cc
+					daddr 127.0.0.1 proto tcp syn dport 9229 REJECT;
+				}
+				""",
+		}
+	end
+end
---
+++ mix.exs
@@ -0,0 +1,21 @@
+defmodule RoleGoogleChrome.Mixfile do
+	use Mix.Project
+
+	def project do
+		[
+			app:             :role_google_chrome,
+			version:         "0.1.0",
+			elixir:          ">= 1.4.0",
+			build_embedded:  Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+			deps:            deps()
+		]
+	end
+
+	defp deps do
+		[
+			{:converge,                ">= 0.1.0"},
+			{:role_google_chrome_repo, ">= 0.1.0"},
+		]
+	end
+end
